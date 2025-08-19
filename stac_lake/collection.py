489054cabf0b7a51ed9b9f06c42a2e6463d7035b
Alex Yeo <ayeo144@googlemail.com>
2025-08-19 17:26:00 +0100

    Run ruff formatting on code.

--- stac_lake/collection.py
+++ stac_lake/collection.py
@@ -6,6 +6,7 @@ import pystac
 
 REQUIREMENT_REGISTRY = {}
 
+
 def register_requirement(cls):
     REQUIREMENT_REGISTRY[cls.__name__] = cls
     return cls
@@ -19,7 +20,9 @@ class ItemRequirementValidationResult:
 
 @dataclass
 class BaseItemRequirement:
-    def validate_item_against_requirement(self, item: pystac.Item) -> ItemRequirementValidationResult:
+    def validate_item_against_requirement(
+        self, item: pystac.Item
+    ) -> ItemRequirementValidationResult:
         raise NotImplementedError
 
     def to_dict(self) -> Dict[str, Any]:
@@ -28,23 +31,27 @@ class BaseItemRequirement:
     @classmethod
     def from_dict(cls, data: Dict[str, Any]):
         raise NotImplementedError
-    
+
 
 @register_requirement
 @dataclass
 class ProjectionRequirement(BaseItemRequirement):
     epsg: Optional[int] = None
 
-    def validate_item_against_requirement(self, item: pystac.Item) -> ItemRequirementValidationResult:
+    def validate_item_against_requirement(
+        self, item: pystac.Item
+    ) -> ItemRequirementValidationResult:
         def _get_epsg_code_int(item: pystac.Item) -> Optional[int]:
             epsg_str = item.properties.get("proj:code", None)
             if epsg_str is None:
-                return 
+                return
             return int(epsg_str.split("EPSG:")[1])
 
         if self.epsg and _get_epsg_code_int(item) != self.epsg:
-            return ItemRequirementValidationResult(False, "Item does not match required EPSG code.")
-        return ItemRequirementValidationResult(True) 
+            return ItemRequirementValidationResult(
+                False, "Item does not match required EPSG code."
+            )
+        return ItemRequirementValidationResult(True)
 
     def to_dict(self):
         return {"type": self.__class__.__name__, "epsg": self.epsg}
@@ -52,7 +59,7 @@ class ProjectionRequirement(BaseItemRequirement):
     @classmethod
     def from_dict(cls, data):
         return cls(epsg=data.get("epsg"))
-    
+
 
 @register_requirement
 @dataclass
@@ -60,15 +67,22 @@ class AssetRequirement(BaseItemRequirement):
     asset_name: str
     media_type: Optional[str] = None
 
-    def validate_item_against_requirement(self, item: pystac.Item) -> ItemRequirementValidationResult:
+    def validate_item_against_requirement(
+        self, item: pystac.Item
+    ) -> ItemRequirementValidationResult:
         asset = item.assets.get(self.asset_name, None)
 
         if asset is None:
-            return ItemRequirementValidationResult(False, f"Item is missing required asset: {self.asset_name}")
-        
+            return ItemRequirementValidationResult(
+                False, f"Item is missing required asset: {self.asset_name}"
+            )
+
         if self.media_type and asset.media_type != self.media_type:
-            return ItemRequirementValidationResult(False, f"Asset '{self.asset_name}' does not match required media type: {self.media_type}")
-        
+            return ItemRequirementValidationResult(
+                False,
+                f"Asset '{self.asset_name}' does not match required media type: {self.media_type}",
+            )
+
         return ItemRequirementValidationResult(True)
 
 
@@ -87,14 +101,16 @@ class Collection:
                 is_valid = False
                 message += f"{result.message} \n"
 
-        return ItemRequirementValidationResult(is_valid=is_valid, message=message.strip())
+        return ItemRequirementValidationResult(
+            is_valid=is_valid, message=message.strip()
+        )
 
     def to_dict(self) -> Dict[str, Any]:
         return {
             "stac_collection": self.stac_collection.to_dict(),
-            "item_requirements": [req.to_dict() for req in self.item_requirements]
+            "item_requirements": [req.to_dict() for req in self.item_requirements],
         }
-    
+
     def from_dict(cls, data: Dict[str, Any]) -> "Collection":
         stac_collection = pystac.Collection.from_dict(data["stac_collection"])
         item_requirements = [
@@ -102,9 +118,11 @@ class Collection:
             for data in data.get("item_requirements", [])
         ]
         return cls(stac_collection=stac_collection, item_requirements=item_requirements)
-    
 
-def validate_item_against_requirements(item: pystac.Item, requirements: List[BaseItemRequirement]) -> ItemRequirementValidationResult:
+
+def validate_item_against_requirements(
+    item: pystac.Item, requirements: List[BaseItemRequirement]
+) -> ItemRequirementValidationResult:
     message = ""
     is_valid = True
 
--- tests/test_collection.py
+++ tests/test_collection.py
@@ -4,13 +4,17 @@ import pystac
 from pystac.extensions.projection import ProjectionExtension
 import pytest
 
-from stac_lake.collection import ProjectionRequirement, AssetRequirement, validate_item_against_requirements
+from stac_lake.collection import (
+    ProjectionRequirement,
+    AssetRequirement,
+    validate_item_against_requirements,
+)
 
 
 @pytest.fixture
 def base_item() -> pystac.Item:
     return pystac.Item(
-        id="test-item", 
+        id="test-item",
         properties={},
         geometry={
             "type": "Polygon",
@@ -22,7 +26,7 @@ def base_item() -> pystac.Item:
                     [10, 20],
                     [10, 10],
                 ]
-            ]
+            ],
         },
         bbox=[10, 10, 20, 20],
         datetime=datetime.datetime(year=2020, month=1, day=1),
@@ -36,9 +40,9 @@ class TestProjectionRequirement:
         item = base_item
         item_proj_ext = ProjectionExtension.ext(item, add_if_missing=True)
         item_proj_ext.epsg = 4326
-        
+
         result = requirement.validate_item_against_requirement(item)
-        
+
         assert result.is_valid
         assert result.message is None
 
@@ -48,9 +52,9 @@ class TestProjectionRequirement:
         item = base_item
         item_proj_ext = ProjectionExtension.ext(item, add_if_missing=True)
         item_proj_ext.epsg = 3786
-        
+
         result = requirement.validate_item_against_requirement(item)
-        
+
         assert not result.is_valid
         assert result.message == "Item does not match required EPSG code."
 
@@ -60,19 +64,22 @@ class TestAssetRequirement:
         requirement = AssetRequirement(asset_name="test_asset", media_type="image/png")
 
         item = base_item
-        item.add_asset("test_asset", pystac.Asset(href="http://example.com/test.png", media_type="image/png"))
+        item.add_asset(
+            "test_asset",
+            pystac.Asset(href="http://example.com/test.png", media_type="image/png"),
+        )
 
         result = requirement.validate_item_against_requirement(item)
-        
+
         assert result.is_valid
 
     def test_validate_item_with_missing_asset(self, base_item):
         requirement = AssetRequirement(asset_name="test_asset")
 
         item = base_item
-        
+
         result = requirement.validate_item_against_requirement(item)
-        
+
         assert not result.is_valid
         assert result.message == "Item is missing required asset: test_asset"
 
@@ -80,27 +87,39 @@ class TestAssetRequirement:
         requirement = AssetRequirement(asset_name="test_asset", media_type="image/png")
 
         item = base_item
-        item.add_asset("test_asset", pystac.Asset(href="http://example.com/test.jpg", media_type="image/jpeg"))
+        item.add_asset(
+            "test_asset",
+            pystac.Asset(href="http://example.com/test.jpg", media_type="image/jpeg"),
+        )
 
         result = requirement.validate_item_against_requirement(item)
-        
+
         assert not result.is_valid
-        assert result.message == "Asset 'test_asset' does not match required media type: image/png"
+        assert (
+            result.message
+            == "Asset 'test_asset' does not match required media type: image/png"
+        )
 
 
 def test_validate_item_against_requirements(base_item):
     item = base_item
-    item.add_asset("test_asset", pystac.Asset(href="http://example.com/test.png", media_type="image/png"))
+    item.add_asset(
+        "test_asset",
+        pystac.Asset(href="http://example.com/test.png", media_type="image/png"),
+    )
     item_proj_ext = ProjectionExtension.ext(item, add_if_missing=True)
     item_proj_ext.epsg = 3786
 
     requirements = [
         ProjectionRequirement(epsg=4326),
-        AssetRequirement(asset_name="test_asset", media_type="image/jpeg")
+        AssetRequirement(asset_name="test_asset", media_type="image/jpeg"),
     ]
 
     result = validate_item_against_requirements(item, requirements)
 
     assert not result.is_valid
     assert "Item does not match required EPSG code." in result.message
-    assert "Asset 'test_asset' does not match required media type: image/jpeg" in result.message
+    assert (
+        "Asset 'test_asset' does not match required media type: image/jpeg"
+        in result.message
+    )
